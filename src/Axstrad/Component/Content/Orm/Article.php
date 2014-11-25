6a14fc2a415ab9c53ce1bd24cde55571d9557baa
Dan Kempster <dev@dankempster.co.uk>
2014-11-25 22:45:51 +0000

    Make Axstad/Content entities implement Axstrad/DoctrineORM's Entity interface.

--- src/Axstrad/Component/Content/Orm/Article.php
+++ src/Axstrad/Component/Content/Orm/Article.php
@@ -2,12 +2,14 @@
 namespace Axstrad\Component\Content\Orm;
 
 use Axstrad\Component\Content\Model\Article as BaseArticle;
+use Axstrad\Component\DoctrineOrm\Entity;
 use Axstrad\Component\DoctrineOrm\Entity\IntegerIdTrait;
 
 /**
  * Axstrad\Component\Content\Orm\Article
  */
-abstract class Article extends BaseArticle
+abstract class Article extends BaseArticle implements
+    Entity
 {
     use IntegerIdTrait;
 }
--- src/Axstrad/Component/Content/Orm/Copy.php
+++ src/Axstrad/Component/Content/Orm/Copy.php
@@ -2,13 +2,15 @@
 namespace Axstrad\Component\Content\Orm;
 
 use Axstrad\Component\Content\Model\Copy as BaseCopy;
+use Axstrad\Component\DoctrineOrm\Entity;
 use Axstrad\Component\DoctrineOrm\Entity\IntegerIdTrait;
 
 
 /**
  * Axstrad\Component\Content\Orm\Copy
  */
-abstract class Copy extends BaseCopy
+abstract class Copy extends BaseCopy implements
+    Entity
 {
     use IntegerIdTrait;
 }
