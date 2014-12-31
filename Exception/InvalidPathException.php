614d915505723f70374370d15dac6361880c68b7
Dan Kempster <dev@dankempster.co.uk>
2014-12-31 01:10:53 +0000

    PathInvaldiException shouldn't extend Axstrad\Common\Exception\InvalidArgumentException

--- Exception/InvalidPathException.php
+++ Exception/InvalidPathException.php
@@ -12,16 +12,30 @@
 
 namespace Axstrad\Component\Filesystem\Exception;
 
+use Exception as BaseException;
+use InvalidArgumentException as BaseInvalidArgumentException;
 
 /**
  * Axstrad\Component\Filesystem\Exception\InvalidPathException
  *
  * @author Dan Kempster <dev@dankempster.co.uk>
  */
-class InvalidPathException extends InvalidArgumentException
+class InvalidPathException extends BaseInvalidArgumentException implements
+    Exception
 {
-    public static function create($path, $code = null, \Exception $previous = null)
-    {
+    /**
+     * Create a new InvalidPathException
+     *
+     * @param string $path The invalid path
+     * @param null|integer $code The error code
+     * @param BaseException|null $previous Previous exception
+     * @return InvalidArgumentException
+     */
+    public static function create(
+        $path,
+        $code = null,
+        BaseException $previous = null
+    ) {
         $class = get_called_class();
         return new $class(
             sprintf("The path '%s' doesn't exist.", $path),
